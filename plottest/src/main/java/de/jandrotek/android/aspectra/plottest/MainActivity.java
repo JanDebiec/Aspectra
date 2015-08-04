7aa12c58498813c7e633cb762c3df30b99ac034e
Jan Debiec <jan.debiec747@gmail.com>
2015-08-04 19:08:36 +0200

    begin with plotTest app

---
+++ plottest/src/main/java/de/jandrotek/android/aspectra/plottest/MainActivity.java
@@ -0,0 +1,46 @@
+package de.jandrotek.android.aspectra.plottest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import de.jandrotek.android.aspectra.core.AspectraGlobals;
+import de.jandrotek.android.aspectra.libplotspectrav3.PlotViewFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static PlotViewFragment mPlotViewFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPlotViewFragment = PlotViewFragment.newInstance(AspectraGlobals.ACT_ITEM_LIVE_VIEW, dummyItems);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fvPlotView, mPlotViewFragment)
+                .commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

46afafb0d2ffaac426a424965234d11deb3e4003
Jan on t530 <jan.debiec747@gmail.com>
2017-10-31 15:56:39 +0100

    after refactoring, app can be build, not tested yet

--- app/src/main/java/de/jandrotek/android/aspectra/main/CameraHandle.java
+++ app/src/main/java/de/jandrotek/android/aspectra/main/CameraHandle.java
@@ -8,6 +8,7 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by jan on 27.10.2017.
@@ -18,6 +19,12 @@ public class CameraHandle {
 
     private Activity mActivity = null;
     private Camera mCamera = null;
+
+    public List<Camera.Size> getSupportedPreviewSizes() {
+        return mSupportedPreviewSizes;
+    }
+
+    private List<Camera.Size> mSupportedPreviewSizes;
     private int mResult;
 
     public int getResult() {
@@ -36,14 +43,14 @@ public class CameraHandle {
 
     }
 
-//    public void setCamera(Camera camera) {
-//        mCamera = camera;
-//        if (mCamera != null) {
-//            mSupportedPreviewSizes = mCamera.getParameters()
-//                    .getSupportedPreviewSizes();
+    public void setCamera(Camera camera) {
+        mCamera = camera;
+        if (mCamera != null) {
+            mSupportedPreviewSizes = mCamera.getParameters()
+                    .getSupportedPreviewSizes();
 //            if (mSurfaceCreated) requestLayout();
-//        }
-//    }
+        }
+    }
 //
 //    public void switchCamera(Camera camera) {
 //        setCamera(camera);
@@ -55,6 +62,11 @@ public class CameraHandle {
 //            }
 //        }
 //    }
+    public void setCameraDisplayOrientation(int cameraId) {
+        mResult = getCameraDegResult(cameraId);
+        mCamera.setDisplayOrientation(mResult);
+    }
+
 
     public  int getCameraDegResult(int cameraId) {
         Camera.CameraInfo info =
@@ -108,4 +120,23 @@ public class CameraHandle {
         }
     }
 
+    public int startPreview(Camera.Size cameraPreviewSize) {
+        int imageFormat = 0;
+        try {
+            Camera.Parameters parameters;
+
+            parameters = mCamera.getParameters();
+            parameters.setPreviewSize(cameraPreviewSize.width, cameraPreviewSize.height);
+            imageFormat = parameters.getPreviewFormat();
+
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY);
+
+            mCamera.startPreview();
+            mCamera.setParameters(parameters);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "Exception caused by setCameraParams()", e);
+        }
+        return imageFormat;
+    }
 }
